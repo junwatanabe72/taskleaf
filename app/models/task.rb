1fa2be64912d5e7b6f67adcd9d2bcbb2f9304e2e
jun.watanabe <junwatanabe72@yahoo.co.jp>
2019-09-26 08:25:02 +0000

    start

---
+++ app/models/task.rb
@@ -0,0 +1,15 @@
+class Task < ApplicationRecord
+    before_validation :set_nameless_name
+    
+    validates :name, presence: true
+    validates :name, length: {maximum: 30}
+    
+    belongs_to :user
+    
+    
+   def set_nameless_name
+     self.name = "名前なし" if name.blank?
+   end
+   
+    
+end
